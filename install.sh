3690f5a38abc941dc0e72cf86142793076e04199
Giorgio Bonfiglio <me@grg.pw>
2019-01-21 14:59:16 +0000

    make sure pip3 is installed before using it

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
+apt install python3-mysqldb python3-pip
 pip3 install python-magic bottle
-apt install python3-mysqldb
 echo "Mysql Login : "
 read mylogin
 mysql -u "$mylogin" -p < setup.sql
